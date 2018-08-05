3b406ff9fbf675390a3bfc9dabccdfc9e412f88f
jivfur <jivfur@gmail.com>
2018-08-05 18:12:20 -0500

    Almost Done

--- 3C/script.py
+++ 3C/script.py
@@ -21,6 +21,52 @@ def CalculateMinNumCoins(m, coins):
 	
 
 
-money = 19508
-coins=[22,11,10,5,3,1]
-print CalculateMinNumCoins(money, coins)
+# money = 19508
+# coins=[22,11,10,5,3,1]
+# print CalculateMinNumCoins(money, coins)
+
+def ManhattanProblem(M,N,Down, Right):
+	S=[[0 for i in range(N+1)]for  j in range(M+1)]
+	for i in range(M): 
+		S[i+1][0]= S[i][0]+Down[i][0]
+	for j in range(N):
+		S[0][j+1]= S[0][j]+Right[0][j]
+	for i in range(M):
+		for j in range(N):
+			S[i+1][j+1]= max(S[i][j+1]+Down[i][j+1], S[i+1][j]+Right[i+1][j])
+	return S[M][N]
+
+def ManhattanProblemDiag(M,N,Down,Right,Diag):
+	S=[[0 for i in range(N+1)]for  j in range(M+1)]
+	for i in range(M): 
+		S[i+1][0]= S[i][0]+Down[i][0]
+	for j in range(N):
+		S[0][j+1]= S[0][j]+Right[0][j]
+	for i in range(M):
+		for j in range(N):
+			S[i+1][j+1]= max(S[i][j+1]+Down[i][j+1], S[i+1][j]+Right[i+1][j],S[i][j]+Diag[i][j])
+	return S[M][N]
+
+l=[int(x) for x in sys.stdin.readline().split()]
+lines = sys.stdin.read().splitlines()
+Down=[]
+for i in range(l[0]):
+	row = [int(x) for x in lines[i].split()]
+	Down.append(row)
+Right=[]
+start=l[0]+1
+for j in range(l[0]+1):
+	row = [int(x) for x in lines[start+j].split()]
+	Right.append(row)
+Diag=[]
+start+=j+2
+for j in range(l[1]):
+	row = [int(x) for x in lines[start+j].split()]
+	Diag.append(row)
+
+print ManhattanProblemDiag(l[0],l[1],Down,Right,Diag)
+
+
+
+
+
