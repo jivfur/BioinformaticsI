9cbe9bdba6ab45e585e7bc203ab37d8f576da884
Ivan Fuentes <jivfur@tamu.edu>
2018-08-06 18:24:06 -0500

    Topological Order

--- 3C/script.py
+++ 3C/script.py
@@ -1,10 +1,6 @@
 import sys
 import os
 
-
-
-
-
 def CalculateMinNumCoins(m, coins):
 	MinNumCoins={0:0}
 	for i in range(1,m+1):
@@ -73,28 +69,80 @@ def pretty(P):
 
 def LCSBackTrack(v, w):
 	lenV = len(v)
-	lenW = len(w)
-	print lenV,lenW
-	S=[[0 for i in range(lenV+2)]for j in range(lenW+1)]
-	B =[[0 for i in range(lenV+1)]for j in range(lenW)]
-	pretty(B)
-	print len(S),len(S[0])
+	lenW = len(w)	
+	S=[[0 for i in range(lenW+1)]for j in range(lenV+1)]
+	B =[[0 for i in range(lenW)]for j in range(lenV)]	
 	for i in range(lenV): #8
 		for j in range(lenW): #9
 			S[i+1][j+1]=max(S[i][j+1],S[i+1][j],(v[i]==w[j])+S[i][j])
-			if S[i+1][j+1]==S[i][j]+2 and v[i]==w[j]:				
-				B[i][j] = "T"
-			elif S[i+1][j+1]==S[i+1][j]:				
-				B[i][j] = "R"
-			elif S[i+1][j+1]==S[i][j+1]:
-				B[i][j] = "D"
-	pretty(S)
-	pretty(B)
+			if S[i+1][j+1]==S[i][j+1]:
+				B[i][j]="D"
+			elif S[i+1][j+1]==S[i+1][j]:
+				B[i][j]="R"
+			elif (S[i+1][j+1]==S[i][j]+1) and v[i]==w[j]:
+				B[i][j]="T"
+	return S,B
 
 
-LCSBackTrack("AACCTTGG","ACACTGTGA")
+# def OutputLCS(backtrack, v, i, j):	
+# 	if i==-1 or j==-1:
+# 		return ""
+# 	elif backtrack[i][j]=="D":
+# 		return OutputLCS(backtrack,v,i-1,j)
+# 	elif backtrack[i][j]=="R":
+# 		return OutputLCS(backtrack,v,i,j-1)
+# 	else:		
+# 		return v[i]+OutputLCS(backtrack,v,i-1,j-1)
+def OutputLCS(backtrack, v, i, j):
+	cad=""
+	while(i>=0 and j>=0):
+		if backtrack[i][j]=="D":
+			i-=1
+		elif backtrack[i][j]=="R":
+			j-=1
+		else:		
+			cad+=v[i]
+			i-=1
+			j-=1
+	return cad
+
+#It should return a list
+def topologicalOrder(graph):
+	
 
 
+
+
+
+
+
+I = int(sys.stdin.readline())
+J = int(sys.stdin.readline())
+lines = sys.stdin.read().splitlines()
+D=abs(I-J)+1
+graph=[[0 for i in range(D)] for j in range(D)]
+for l in lines:
+	temp = l.split("->")
+	temp2 = temp[1].split(":")
+	graph[int(temp[0])][int(temp2[0])]=int(temp2[1])
+tp=topologicalOrder(graph)
+
+
+
+# lines = sys.stdin.read().splitlines()
+# w=lines[0].strip()
+# v=lines[1].strip()
+# S,B=LCSBackTrack(v,w)
+# # pretty(S)
+# # pretty(B)
+# print OutputLCS(B,v,len(v)-1,len(w)-1,)[::-1]
+
+# if S[i+1][j+1]==S[i][j]+2 and v[i]==w[j]:				
+# 				B[i][j] = "T"
+# 			elif S[i+1][j+1]==S[i+1][j]:				
+# 				B[i][j] = "R"
+# 			elif S[i+1][j+1]==S[i][j+1]:
+# 				B[i][j] = "D"
 
 
 
